bb6dc79b21e343b24b306ccd48509f952bac3601
Emil Axelsson <emil.axelsson@liu.se>
2017-02-17 17:51:13 +0100

    Replace tabs by spaces

--- webgui/build.js
+++ webgui/build.js
@@ -9,22 +9,22 @@ const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const webpackConfig = require('./webpack.production.config');
 
 const executable = {
-	win32: 'server.exe',
+    win32: 'server.exe',
 }[os.platform()] || 'server';
 
 rimraf('dist', () => {
-	mkdirp('dist', () => {
-		const compiler = webpack(webpackConfig);
+    mkdirp('dist', () => {
+        const compiler = webpack(webpackConfig);
 
-		compiler.apply(new ProgressBarPlugin({
-  			format: '  build [:bar] :percent (:elapsed seconds)',
-  			clear: false
-		}));
+        compiler.apply(new ProgressBarPlugin({
+            format: '  build [:bar] :percent (:elapsed seconds)',
+            clear: false
+        }));
 
-		compiler.run(() => {
-			cp.sync('backend/config.json', 'dist/config.json');
-		});
+        compiler.run(() => {
+            cp.sync('backend/config.json', 'dist/config.json');
+        });
 
-		enclose(['--output', 'dist/' + executable, 'backend/server.js']);
-	});
+        enclose(['--output', 'dist/' + executable, 'backend/server.js']);
+    });
 });
