49d1ef2aadc115b00b442f51c6393ddb389f73a4
Alexander Bock <mail@alexanderbock.eu>
2020-09-30 18:38:38 +0200

    Add missing file

---
+++ common/src/logconfiguration.cpp
@@ -0,0 +1,59 @@
+/*****************************************************************************************
+ *                                                                                       *
+ * Copyright (c) 2016 - 2020                                                             *
+ * Alexander Bock, Erik Sundén, Emil Axelsson                                            *
+ *                                                                                       *
+ * All rights reserved.                                                                  *
+ *                                                                                       *
+ * Redistribution and use in source and binary forms, with or without modification, are  *
+ * permitted provided that the following conditions are met:                             *
+ *                                                                                       *
+ * 1. Redistributions of source code must retain the above copyright notice, this list   *
+ *    of conditions and the following disclaimer.                                        *
+ *                                                                                       *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this     *
+ *    list of conditions and the following disclaimer in the documentation and/or other  *
+ *    materials provided with the distribution.                                          *
+ *                                                                                       *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be  *
+ *    used to endorse or promote products derived from this software without specific    *
+ *    prior written permission.                                                          *
+ *                                                                                       *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY   *
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  *
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT   *
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,        *
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED  *
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR    *
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN      *
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    *
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH   *
+ * DAMAGE.                                                                               *
+ *                                                                                       *
+ ****************************************************************************************/
+
+#include "logconfiguration.h"
+
+namespace {
+    constexpr const char* KeyLogRotationFrequency = "frequency";
+    constexpr const char* KeyLogRotationKeepPrevious = "keepPrevious";
+} // namespace
+
+namespace common {
+
+void to_json(nlohmann::json& j, const LogRotation& lr) {
+    j[KeyLogRotationFrequency] = lr.frequency.count();
+    j[KeyLogRotationKeepPrevious] = lr.keepPrevious;
+}
+
+void from_json(const nlohmann::json& j, LogRotation& lr) {
+    if (j.find(KeyLogRotationFrequency) != j.end()) {
+        int hs = j[KeyLogRotationFrequency].get<int>();
+        lr.frequency = std::chrono::hours(hs);
+    }
+    if (j.find(KeyLogRotationKeepPrevious) != j.end()) {
+        j[KeyLogRotationKeepPrevious].get_to(lr.keepPrevious);
+    }
+}
+
+} // namespace common
