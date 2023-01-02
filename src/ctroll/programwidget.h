2913a5ad6d64296df948fac674eb6c33c68e26c3
Alexander Bock <alexander.bock@liu.se>
2023-01-02 11:27:12 +0100

    Reformatting copyright header

--- src/ctroll/programwidget.h
+++ src/ctroll/programwidget.h
@@ -1,6 +1,6 @@
 /*****************************************************************************************
  *                                                                                       *
- * Copyright (c) 2016 - 2023                                                             *
+ * Copyright (c) 2016-2023                                                             *
  * Alexander Bock, Erik Sunden, Emil Axelsson                                            *
  *                                                                                       *
  * All rights reserved.                                                                  *
